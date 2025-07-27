e8bae76a418f9172b26c54a13c5bbc333d2948ff
TranPhat-28 <tphat8301@gmail.com>
2025-07-27 17:34:37 +0700

    feat: Add Service Response

---
+++ Models/ServiceResponse.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pollkit_server.Models
+{
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+        public string Message { get; set; } = "";
+        public bool IsSuccess { get; set; } = true;
+    }
+}
